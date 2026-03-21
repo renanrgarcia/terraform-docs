3208a73236ad1f94cd945bcaa8923f8426af020c
Renan Garcia <renanrgarcia@gmail.com>
2026-03-20 21:13:43 -0300

    Add initial Terraform configuration and provider lock file

---
+++ lab1/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.65.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-storage-tf"
+  location = "South Africa North"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "terraformtf65842"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tf-state-container" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.example.id
+  container_access_type = "private"
+}
